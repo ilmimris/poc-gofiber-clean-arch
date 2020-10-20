6e85f454a603efb17272f47931b9430de8d187a3
Muhammad Rafiul Ilmi Syarifudin <mr.is@arc.itb.ac.id>
2020-10-20 10:02:32 +0700

    add switch between mysql and postgres

--- api/app.go
+++ api/app.go
@@ -10,12 +10,18 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	_authorRepo "github.com/ilmimris/poc-gofiber-clean-arch/pkg/author/repository/psql"
+	_authorRepoMysql "github.com/ilmimris/poc-gofiber-clean-arch/pkg/author/repository/mysql"
+	"github.com/ilmimris/poc-gofiber-clean-arch/pkg/domain"
+
+	_authorRepoPsql "github.com/ilmimris/poc-gofiber-clean-arch/pkg/author/repository/psql"
 	_postDelivery "github.com/ilmimris/poc-gofiber-clean-arch/pkg/post/delivery/rest"
-	_postRepo "github.com/ilmimris/poc-gofiber-clean-arch/pkg/post/repository/psql"
+	_postRepoMysql "github.com/ilmimris/poc-gofiber-clean-arch/pkg/post/repository/mysql"
+
+	_postRepoPsql "github.com/ilmimris/poc-gofiber-clean-arch/pkg/post/repository/psql"
 	_postUsecase "github.com/ilmimris/poc-gofiber-clean-arch/pkg/post/usecase"
 	"github.com/spf13/viper"
 
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 )
 
@@ -31,16 +37,20 @@ func init() {
 	}
 }
 
-func databaseConnection() (*sql.DB, error) {
+func databaseConnectionPsql() (*sql.DB, error) {
 	// Read db configuration
 	dbHost := viper.GetString(`database.host`)
 	dbPort := viper.GetString(`database.port`)
 	dbUser := viper.GetString(`database.user`)
 	dbPass := viper.GetString(`database.pass`)
 	dbName := viper.GetString(`database.name`)
-	connection := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+
 	val := url.Values{}
+
+	// Config db postgres
+	connection := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
 	val.Add("sslmode", "disable")
+
 	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
 
 	// Connect with database
@@ -58,8 +68,50 @@ func databaseConnection() (*sql.DB, error) {
 	return dbConn, err
 }
 
+func databaseConnectionMysql() (*sql.DB, error) {
+	// Read db configuration
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+
+	val := url.Values{}
+
+	// Config db mysql
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Jakarta")
+
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+
+	// Connect with database
+	dbConn, err := sql.Open(`mysql`, dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = dbConn.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dbConn, err
+}
+
 func main() {
-	db, err := databaseConnection()
+	dbKind := viper.GetString(`database.kind`)
+
+	var db *sql.DB
+	var err error
+
+	switch dbKind {
+	case "mysql":
+		db, err = databaseConnectionMysql()
+	case "postgres":
+		db, err = databaseConnectionPsql()
+	}
 	defer func() {
 		err := db.Close()
 		if err != nil {
@@ -71,8 +123,18 @@ func main() {
 	}
 	fmt.Println("Database connection success!")
 
-	postRepo := _postRepo.NewPsqlPostRepository(db)
-	authorRepo := _authorRepo.NewPsqlAuthorRepository(db)
+	var postRepo domain.PostRepository
+	var authorRepo domain.AuthorRepository
+
+	switch dbKind {
+	case "mysql":
+		postRepo = _postRepoMysql.NewMysqlPostRepository(db)
+		authorRepo = _authorRepoMysql.NewMysqlAuthorRepository(db)
+	case "postgres":
+		postRepo = _postRepoPsql.NewPsqlPostRepository(db)
+		authorRepo = _authorRepoPsql.NewPsqlAuthorRepository(db)
+	}
+
 	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
 
 	postUcase := _postUsecase.NewPostUsecase(postRepo, authorRepo, timeoutContext)
@@ -90,6 +152,6 @@ func main() {
 
 	_postDelivery.NewPostHandler(app, postUcase)
 
-	log.Fatal(app.Listen(":8080"))
+	log.Fatal(app.Listen(viper.GetString(`server.address`)))
 
 }
